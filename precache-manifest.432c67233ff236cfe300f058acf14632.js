2756ff37fd5f01a398b8770f75585ab469b21770
jibaro <jessiegmatias@gmail.com>
2020-06-13 23:01:38 -0400

    Updates

---
+++ precache-manifest.432c67233ff236cfe300f058acf14632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eec5cd4ceee598a489186e66939cdd9",
+    "url": "/covid-data/index.html"
+  },
+  {
+    "revision": "fd9db9d996289cbca675",
+    "url": "/covid-data/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "fd9db9d996289cbca675",
+    "url": "/covid-data/static/js/2.299d9062.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/covid-data/static/js/2.299d9062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f494265539bde3fe477",
+    "url": "/covid-data/static/js/main.ee4e9c5d.chunk.js"
+  },
+  {
+    "revision": "35074eba94e54f40aded",
+    "url": "/covid-data/static/js/runtime-main.6299a9ab.js"
+  }
+]);
